dc6a59ebd3f13f2067e902f83c434069e1834e57
Ken Duigraha Putra <email": null>
2016-11-12 22:46:37 +0700

    added skills html, updated script.js, installed purl

--- client/js/script.js
+++ client/js/script.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
   changeTitle()
+  changeTitleSkills()
 })
+
+function changeTitleSkills(){
+  $('#btn_submit_skill').on('click', function(){
+    $.urlParam = function(name){
+    	var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    	return results[1] || 0;
+    }
+
+    var new_title = $.urlParam('title').split('%20').join(' ')
+    console.log(new_title);
+
+    $('#title_skills').text(new_title)
+  })
+}
 
 function changeTitle(){
   $('#btn_submit').on('click', function(e){
