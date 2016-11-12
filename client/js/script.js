2003da9bd7592fc87f5276264d5212dae68f9df9
Ken Duigraha Putra <email": null>
2016-11-12 22:29:22 +0700

    updated index.html feature

--- client/js/script.js
+++ client/js/script.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-
+  changeTitle()
 })
+
+function changeTitle(){
+  $('#btn_submit').on('click', function(e){
+    e.preventDefault()
+
+    var new_title = $('#title').val()
+    $('#data-heading').text(new_title)
+  })
+}
